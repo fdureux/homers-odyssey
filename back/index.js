819cafef4efd1301ed569fc20e8d985889736fea
fdureux <fanny.dureux@gmail.com>
2020-12-15 12:13:54 +0100

    create auth folder and signup route

--- back/index.js
+++ back/index.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const port = process.env.PORT || 8080;
 const app = express();
 
+const authRouter = require("./routes/auth/auth");
+
+app.use("/auth", authRouter);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- back/routes/index.js
+++ back/routes/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const authRouter = require("./auth/auth");
+const app = express();
+
+app.use("/auth", authRouter);
 
 router.get("/", (req, res) => {
   res.send("youhou");
